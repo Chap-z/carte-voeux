3f1c9826fe5a659d59ab9e9adcb3b2fc6e2b134f
Chap-z <cha.lacroix@hotmail.fr>
2018-05-03 14:24:41 +0200

    night/day

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,35 @@ new WOW().init();
 
 const moon = document.getElementById('moon');
 const body = document.getElementById('wrap-body');
+const clouds = document.getElementById('cloud');
+var clic = 0;
 
 
 moon.addEventListener('click', nightToDay, false);
 
 
-function nightToDay(){
-    
-    moon.removeAttribute("class", "moon");
-    moon.setAttribute("class", "sun");
+function nightToDay() {
 
-    body.removeAttribute("class", "background-night");
-    body.setAttribute("class", "background-sun");
 
+
+    console.log(clic);
+
+    if (clic == 0) {
+
+        moon.removeAttribute("class", "moon");
+        moon.setAttribute("class", "sun");
+
+        body.removeAttribute("class", "background-night");
+        body.setAttribute("class", "background-sun");
+
+        clic ++;
+    } else{
+        moon.removeAttribute("class", "sun");
+        moon.setAttribute("class", "moon");
+
+        body.removeAttribute("class", "background-sun");
+        body.setAttribute("class", "background-night");
+
+        clic = 0;
+    }
 }
