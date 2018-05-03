6e5c215e26c6b9991ac450059de0cefc2abaacdb
Chap-z <cha.lacroix@hotmail.fr>
2018-05-03 11:38:46 +0200

    tentative noght/dauyy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
+new WOW().init();
+
 const moon = document.getElementById('moon');
+const body = document.getElementById('wrap-body');
+
 
 moon.addEventListener('click', nightToDay, false);
 
 
 function nightToDay(){
+    
     moon.removeAttribute("class", "moon");
     moon.setAttribute("class", "sun");
-    
+
+    body.removeAttribute("class", "background-night");
+    body.setAttribute("class", "background-sun");
 
 }
